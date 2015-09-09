0d461165e8db8dea744e86bc061282dc87cbed33
Haibo <apolorwang@gmail.com>
2015-09-09 14:37:43 +0800

    extract the gps data in format of: lat,lng

--- Data/Uncompressed/extratlat.py
+++ Data/Uncompressed/extratlat.py
@@ -4,4 +4,4 @@ import numpy as np
 
 traj = np.genfromtxt('cycling_trajectory_20120916_elevation.txt', delimiter=',')
 with open('cycling_trajectory_20120916.txt', 'w') as f:
-    [f.write(str(L[0])+','+str(L[1])+'\n') for L in traj]
+    [f.write(str(L[1])+','+str(L[0])+'\n') for L in traj]
