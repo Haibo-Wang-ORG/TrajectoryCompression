30df9de451c41d969a82b9cfccd643922fab8186
Haibo <apolorwang@gmail.com>
2015-09-10 19:27:21 +0800

    Add comments to test_dp.py

--- test_dp.py
+++ test_dp.py
@@ -14,12 +14,15 @@ class DPTestCase(unittest.TestCase):
     _dp = DouglasPeucker()
     
     def test_is_uncompressed_douglas_peucker(self):
+        """Does the algorithm compress no data when the delta is 0?"""
         self.assertEqual(range(len(self._Q)), self._dp._douglas_peucker(self._Q, 0, len(self._Q)-1, 0))
         
     def test_is_maximum_compressed_douglas_peucker(self):
+        """Does the algorithm compress all data except the two ends when the delta is +infinity?"""
         self.assertEqual([0,len(self._Q)-1], self._dp._douglas_peucker(self._Q, 0, len(self._Q)-1, float('inf')))
         
     def test_is_partially_compressed_douglas_peucker(self):
+        """Does the algorithm partially compress the data when the delta is 1.5?"""
         self.assertEqual([0,2,len(self._Q)-1], self._dp._douglas_peucker(self._Q, 0, len(self._Q)-1, 1.5))
         
 if __name__ == '__main__':
